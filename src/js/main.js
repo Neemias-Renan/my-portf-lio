6a1b6f6043455d10555b913048356f7561f9d7ae
Neemias Renan <69238611+Neemias-Renan@users.noreply.github.com>
2021-09-29 20:35:49 -0300

    Adicionei o arquivo JS

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+function abrirMenu(){
+    document.getElementById("menu").style.display = "block"
+    document.getElementById("BTN_fecharMenu").style.display = "block"
+    document.getElementById("BTN_abrirMenu").style.display = "none"
+}
+
+function fecharMenu(){
+    document.getElementById("menu").style.display = "none"
+    document.getElementById("BTN_fecharMenu").style.display = "none"
+    document.getElementById("BTN_abrirMenu").style.display = "block"
+}
