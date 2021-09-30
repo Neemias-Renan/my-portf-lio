a306b0eea61cb1f06aae74a0d32baf8cafb225ac
Neemias Renan <69238611+Neemias-Renan@users.noreply.github.com>
2021-09-29 21:02:58 -0300

    Atualização de responsividade

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ function abrirMenu(){
     document.getElementById("menu").style.display = "block"
     document.getElementById("BTN_fecharMenu").style.display = "block"
     document.getElementById("BTN_abrirMenu").style.display = "none"
+    document.getElementById("body").style.overflow = "hidden"
 }
 
 function fecharMenu(){
     document.getElementById("menu").style.display = "none"
     document.getElementById("BTN_fecharMenu").style.display = "none"
     document.getElementById("BTN_abrirMenu").style.display = "block"
+    document.getElementById("body").style.overflow = "auto"
 }
